27c2ea0103f9da67b4a607c9b90f0bdcf16020db
Karl Xu <karl.yogi.xu@gmail.com>
2018-11-24 22:07:07 +0800

    debug docker push

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "$DOCKER_USERNAME"/$1
+
+LATEST_IMAGE="$DOCKER_USERNAME"/$1
+DATE_IMAGE="$DOCKER_USERNAME"/$1:`date +%Y-%m-%d`
+
+docker push $LATEST_IMAGE
+
 echo `date +%Y-%m-%d`
-docker push "$DOCKER_USERNAME"/$1:`date +%Y-%m-%d`
+dokcer tag  $LATEST_IMAGE $DATE_IMAGE
+docker push $DATE_IMAGE
