993955b31021fdc342dd651bae167ae6c34285e3
Karl Xu <karl.yogi.xu@gmail.com>
2018-11-24 21:55:06 +0800

    debug docker push

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-# docker push $1/$2
-# echo `date +%Y-%m-%d`
-# docker push $1/$2:`date +%Y-%m-%d`
+docker push "$DOCKER_USERNAME"/$1
+echo `date +%Y-%m-%d`
+docker push "$DOCKER_USERNAME"/$1:`date +%Y-%m-%d`
