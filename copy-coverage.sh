b9c6ad1bf9a9f976520e7478acafd15de75f16b5
Karl Xu <karl.yogi.xu@gmail.com>
2018-12-15 01:47:26 +0800

    fix permission error

--- copy-coverage.sh
+++ copy-coverage.sh
@@ -5,6 +5,6 @@ CONTAINER_NAME=$(basename $(pwd))
 
 docker run -i -v $(pwd)/coverage:/tmp/coverage --name $CONTAINER_NAME --rm $DOCKER_IMAGE /bin/sh << COMMANDS
 ls ./coverage
-chmod 777 ./coverage
+chmod -R 777 ./coverage
 mv ./coverage/* /tmp/coverage
 COMMANDS
