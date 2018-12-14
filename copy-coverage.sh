d969bd9890028a07c42c5a26ba876437e7b6b0e5
Karl Xu <karl.yogi.xu@gmail.com>
2018-12-15 01:56:19 +0800

    add detailed list

--- copy-coverage.sh
+++ copy-coverage.sh
@@ -4,7 +4,7 @@ DOCKER_IMAGE=$(basename $(pwd)):test
 CONTAINER_NAME=$(basename $(pwd))
 
 docker run -i -v $(pwd)/coverage:/tmp/coverage --name $CONTAINER_NAME --rm $DOCKER_IMAGE /bin/sh << COMMANDS
-ls ./coverage
+ls -lat ./coverage
 chmod -R 777 ./coverage
 mv ./coverage/* /tmp/coverage
 COMMANDS
