131c864b44db9de4c822e2be259837db400ea6e2
Karl Xu <karl.yogi.xu@gmail.com>
2018-12-15 02:48:45 +0800

    fix recursive chown

--- copy-coverage.sh
+++ copy-coverage.sh
@@ -6,7 +6,7 @@ CONTAINER_NAME=$(basename $(pwd))
 docker run -i -v $(pwd)/coverage:/tmp/coverage --name $CONTAINER_NAME --rm $DOCKER_IMAGE /bin/sh << COMMANDS
 ls -lat ./coverage
 chmod -R 777 ./coverage
-chown $(id -u):$(id -g) ./coverage
+chown -R $(id -u):$(id -g) ./coverage
 ls -lat ./coverage
 mv ./coverage/* /tmp/coverage
 COMMANDS
