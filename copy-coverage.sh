6c2c30d595552ac3445cdc71a6307b567101ee73
Karl Xu <karl.yogi.xu@gmail.com>
2018-12-15 01:58:44 +0800

    fix error

--- copy-coverage.sh
+++ copy-coverage.sh
@@ -6,5 +6,6 @@ CONTAINER_NAME=$(basename $(pwd))
 docker run -i -v $(pwd)/coverage:/tmp/coverage --name $CONTAINER_NAME --rm $DOCKER_IMAGE /bin/sh << COMMANDS
 ls -lat ./coverage
 chmod -R 777 ./coverage
+ls -lat ./coverage
 mv ./coverage/* /tmp/coverage
 COMMANDS
