e71ebd0cf1ef2c7c1e0a2dd12c7cf4e46a9f22ed
Karl Xu <karl.yogi.xu@gmail.com>
2018-12-15 02:57:29 +0800

    fix directory permission error

--- copy-coverage.sh
+++ copy-coverage.sh
@@ -2,11 +2,13 @@
 
 DOCKER_IMAGE=$(basename $(pwd)):test
 CONTAINER_NAME=$(basename $(pwd))
+COVERAGE_REPORT=coverage
 
-docker run -i -v $(pwd)/coverage:/tmp/coverage --name $CONTAINER_NAME --rm $DOCKER_IMAGE /bin/sh << COMMANDS
+mkdir $COVERAGE_REPORT
+
+docker run -i -v $(pwd)/${COVERAGE_REPORT}:/tmp/coverage --name $CONTAINER_NAME --rm $DOCKER_IMAGE /bin/sh << COMMANDS
 ls -lat ./coverage
 chmod -R 777 ./coverage
-chown -R $(id -u):$(id -g) ./coverage
 ls -lat ./coverage
 mv ./coverage/* /tmp/coverage
 COMMANDS
